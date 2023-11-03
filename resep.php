6ebf8671fb17c6a622800308785efa4ac11886a0
RumaSU <r.maulanah19@gmail.com>
2023-11-04 03:43:15 +0700

    Commit resep.php

---
+++ resep.php
@@ -0,0 +1,11 @@
+<?php
+echo "Resep krabby patty";
+echo "------------------";
+echo "1. Bawang";
+echo "2. Ketimun";
+echo "3. Selada";
+echo "4. Tomat";
+echo "5. Daging";
+echo "6. Keju";
+echo "7. Mayonais";
+echo "8. Roti";
